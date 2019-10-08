ca039c32c2b36af0e41e44eb739ed16f07d4900d
Henry Lee <gbvrcx@gmail.com>
2019-10-08 21:22:12 +0900

    basics

---
+++ network.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class AdaIN(nn.Module):
+  def __init__(self):
+    super(self, AdaIN).__init__()
+
+  def forward(self):
+    pass
+
+  def save(self):
+    pass
+
+  def load(self):
+    pass
---
+++ test.py
@@ -0,0 +1,4 @@
+import argparse
+
+from network import AdaIN
+
