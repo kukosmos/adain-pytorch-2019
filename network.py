23c774679e5a49e4c7e397c9823093b21689127c
Henry Lee <gbvrcx@gmail.com>
2019-10-08 21:54:58 +0900

    encoder decoder class

--- network.py
+++ network.py
@@ -2,6 +2,20 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class Encoder(nn.Module):
+  def __init__(self):
+    super(self, Encoder).__init__()
+
+  def forward(self):
+    pass
+
+class Decoder(nn.Module):
+  def __init__(self):
+    super(self, Decoder).__init__()
+
+  def forward(self):
+    pass
+
 class AdaIN(nn.Module):
   def __init__(self):
     super(self, AdaIN).__init__()
