c7ffdafad71478d5d80bfa933aa56a8e58ad2445
Henry Lee <gbvrcx@gmail.com>
2019-10-11 17:08:26 +0900

    coco download script

---
+++ download.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$1" = "" -o "$1" = "-h" -o "$1" = "--help" ]; then
+  echo "Usage: ./download.sh <dataset> [<dataset> ...]"
+  echo ""
+  echo "Script for downloading image datasets"
+  echo ""
+  echo "Positional arguments:"
+  echo "  <dataset> [<dataset> ...]"
+  echo "                        Target dataset to dowload. Supported datasets: coco2014train"
+  echo "                        coco2014test, coco2017train, coco2017test"
+else
+  for var in "$@"; do
+    case $var in
+    "coco2014train")
+      wget -O coco2014train.zip http://images.cocodataset.org/zips/train2014.zip;;
+    "coco2014test")
+      wget -O coco2014test.zip http://images.cocodataset.org/zips/test2014.zip;;
+    "coco2017train")
+      wget -O coco2017train.zip http://images.cocodataset.org/zips/train2017.zip;;
+    "coco2017test")
+      wget -O coco2017test.zip http://images.cocodataset.org/zips/test2017.zip;;
+    *)
+      echo "$var not supported";;
+    esac
+  done
+fi
