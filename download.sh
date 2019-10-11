49f937843406e737d14ec11d44875cd61ad98f80
Henry Lee <gbvrcx@gmail.com>
2019-10-11 17:37:24 +0900

    base dir

--- download.sh
+++ download.sh
@@ -10,18 +10,20 @@ if [ "$1" = "" -o "$1" = "-h" -o "$1" = "--help" ]; then
   echo "                        Target dataset to dowload. Supported datasets: coco2014train"
   echo "                        coco2014test, coco2017train, coco2017test, wikiart"
 else
+  basedir=data
+  mkdir -p $basedir
   for var in "$@"; do
     case $var in
     "coco2014train")
-      wget -O coco2014train.zip http://images.cocodataset.org/zips/train2014.zip;;
+      wget -O $basedir/coco2014train.zip http://images.cocodataset.org/zips/train2014.zip;;
     "coco2014test")
-      wget -O coco2014test.zip http://images.cocodataset.org/zips/test2014.zip;;
+      wget -O $basedir/coco2014test.zip http://images.cocodataset.org/zips/test2014.zip;;
     "coco2017train")
-      wget -O coco2017train.zip http://images.cocodataset.org/zips/train2017.zip;;
+      wget -O $basedir/coco2017train.zip http://images.cocodataset.org/zips/train2017.zip;;
     "coco2017test")
-      wget -O coco2017test.zip http://images.cocodataset.org/zips/test2017.zip;;
+      wget -O $basedir/coco2017test.zip http://images.cocodataset.org/zips/test2017.zip;;
     "wikiart")
-      wget -O wikiart.zip http://web.fsktm.um.edu.my/~cschan/source/ICIP2017/wikiart.zip
+      wget -O $basedir/wikiart.zip http://web.fsktm.um.edu.my/~cschan/source/ICIP2017/wikiart.zip
     *)
       echo "$var not supported";;
     esac
