8dad1bcbf6a2a8ec8c50983ef8777cb921fd8f64
Henry Lee <gbvrcx@gmail.com>
2019-10-14 02:08:08 +0900

    comments for training script

--- train.py
+++ train.py
@@ -4,9 +4,11 @@ import argparse
 
 parser = argparse.ArgumentParser(description='AdaIN Training Script')
 
+# necessary arguments
 parser.add_argument('-cd', '--content-dir', type=str, metavar='<dir>', required=True, help='Directory with content images')
 parser.add_argument('-sd', '--style-dir', type=str, metavar='<dir>', required=True, help='Directory with style images')
 
+# optional arguments for training
 parser.add_argument('--save-dir', type=str, metavar='<dir>', default='./experiments', help='Directory to save trained models, default=./experiments')
 parser.add_argument('--log-dir', type=str, metavar='<dir>', default='./logs', help='Directory to save logs, default=./logs')
 parser.add_argument('--save-interval', type=int, metavar='<int>', default=10000, help='Period for saving model, default=10000')
@@ -14,6 +16,7 @@ parser.add_argument('--include-encoder', action='store_true', help='Option for s
 parser.add_argument('--cuda', action='store_true', help='Option for using GPU if available')
 parser.add_argument('--n-threads', type=int, metavar='<int>', default=2, help='Number of threads used for dataloader, default=2')
 
+# hyper-parameters
 parser.add_argument('--learning-rate', type=float, metavar='<float>', default=1e-4, help='Learning rate, default=1e-4')
 parser.add_argument('--learning-rate-decay', type=float, metavar='<float>', default=5e-5, help='Learning rate decay, default=5e-5')
 parser.add_argument('--max-iter', type=int, metavar='<int>', default=160000, help='Maximun number of iteration, default=160000')
@@ -35,48 +38,62 @@ from tensorboardX import SummaryWriter
 from tqdm import tqdm
 from utils import learning_rate_decay
 
+# for handling errors
 Image.MAX_IMAGE_PIXELS = None
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
+# use gpu if possible
 device = torch.device('cuda' if args.cuda and torch.cuda.is_available() else 'cpu')
 
+# directory trained models
 save_dir = Path(args.save_dir)
 save_dir.mkdir(exist_ok=True, parents=True)
+# directory for logs
 log_dir = Path(args.log_dir)
 log_dir.mkdir(exist_ok=True, parents=True)
 
+# content dataset
 content_dataset = ImageFolderDataset(args.content_dir, train_transform((512, 512), 256))
 content_iter = iter(data.DataLoader(content_dataset, batch_size=args.batch_size, sampler=InfiniteSampler(len(content_dataset)), num_workers=args.n_threads))
+# style dataset
 style_dataset = ImageFolderDataset(args.style_dir, train_transform((512, 512), 256))
 style_iter = iter(data.DataLoader(style_dataset, batch_size=args.batch_size, sampler=InfiniteSampler(len(style_dataset)), num_workers=args.n_threads))
 
+# AdaIN model
 model = AdaIN()
 model.to(device)
 optimizer = torch.optim.Adam(model.decoder.parameters(), lr=args.learning_rate)
 
+# log writer
 writer = SummaryWriter(log_dir=str(log_dir))
 
+# for maximum iteration
 for i in tqdm(range(args.max_iter)):
+  # adjust learning rate
   lr = learning_rate_decay(args.learning_rate, args.learning_rate_decay, i)
   for group in optimizer.param_groups:
     group['lr'] = lr
-  content_images = next(content_iter).to(device)
-  
-  style_images = next(style_iter).to(device)
-  content_images = next(content_iter).to(device)
 
+  # get images
+  content_images = next(content_iter).to(device)
+  style_images = next(style_iter).to(device)
+
+  # calculate loss
   _, loss_content, loss_style = model(content_images, style_images)
   loss_content = args.content_weight * loss_content
   loss_style = args.style_weight * loss_style
   loss = loss_content + loss_style
 
+  # optimize the network
   optimizer.zero_grad()  
   loss.backward()
   optimizer.step()
 
+  # write logs
   writer.add_scalar('loss_content', loss_content.item(), i + 1)
   writer.add_scalar('loss_style', loss_style.item(), i + 1)
 
+  # save model
   if (i + 1) % args.save_interval == 0 or (i + 1) == args.max_iter:
     save_AdaIn(model, os.path.join(save_dir, 'iter_{}.pth'.format(i + 1)), include_encoder=args.include_encoder)
 
