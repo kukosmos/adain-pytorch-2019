aa2e853159f74b0bbb6847911d4fa14740709460
Henry Lee <gbvrcx@gmail.com>
2019-10-14 02:49:48 +0900

    comments for network

--- network.py
+++ network.py
@@ -5,6 +5,7 @@ import torchvision.models as models
 from utils import adaptive_instance_normalization as adain
 from utils import calc_mean_std
 
+# encoder using vgg19 structure
 class Encoder(nn.Module):
   def __init__(self, pretrained=True):
     super(Encoder, self).__init__()
@@ -17,6 +18,7 @@ class Encoder(nn.Module):
     self.enc_2 = nn.Sequential(*features[7:12])
     self.enc_3 = nn.Sequential(*features[12:21])
 
+  # use four internal feature vectors of encoder
   def forward(self, x):
     output_0 = self.enc_0(x)
     output_1 = self.enc_1(output_0)
@@ -25,6 +27,7 @@ class Encoder(nn.Module):
 
     return output_0, output_1, output_2, output_3
 
+# decoder that mirrors encoder
 class Decoder(nn.Module):
   def __init__(self):
     super(Decoder, self).__init__()
@@ -92,6 +95,7 @@ class Decoder(nn.Module):
 
     return x
 
+# main architecture
 class AdaIN(nn.Module):
   def __init__(self, torchvision_encoder=True, training_mode=True):
     super(AdaIN, self).__init__()
@@ -100,32 +104,43 @@ class AdaIN(nn.Module):
     self.decoder = Decoder()
     self.mse_loss = nn.MSELoss()
 
+    # if training mode, calcuate loss, otherwise, not
     self.training_mode = training_mode
 
+    # do not modify parameters from encoder
     for p in self.encoder.parameters():
       p.requires_grad = False
 
   def forward(self, content, style, alpha=1.0, interpolation_weights=None):
     assert 0 <= alpha <= 1, '"alpha" should be between 0 and 1'
+    assert interpolation_weights is None or not self.training_mode, 'Interpolation is not supported while training'
 
+    # get the features from the content image and the style image
     f_content = self.encoder(content)[-1]
     f_style = self.encoder(style)
     if interpolation_weights is not None:
+      # mix the features of style images with interpolation weights
       t = adain(f_content.expand_as(f_style), f_style[-1])
       t = torch.mm(interpolation_weights, t)
     else:
       t = adain(f_content, f_style[-1])
+    # adjust amount of stylization with alpha
     t = alpha * t + (1 - alpha) * f_content
 
+    # create image
     g = self.decoder(t)
 
+    # return image if not training
     if not self.training_mode:
       return g
 
+    # get the features from generated image
     f_g = self.encoder(g)
 
+    # calculate content loss
     l_content = self.mse_loss(f_g[-1], t)
     
+    # calculate style loss
     l_style = 0
     for i in range(4):
       mean_s, std_s = calc_mean_std(f_style[i])
@@ -134,28 +149,40 @@ class AdaIN(nn.Module):
 
     return g, l_content, l_style
 
+# save the model
 def save_AdaIn(model, path='AdaIN.pth', include_encoder=False):
   state_dict = {}
 
+  # include encoder if specified
   if include_encoder:
+    # get the states of encoder
     encoder_dict = model.encoder.state_dict()
     for key in encoder_dict.keys():
       encoder_dict[key] = encoder_dict[key].to(torch.device('cpu'))
     state_dict['encoder'] = encoder_dict
   
+  # get the states of decoder
   decoder_dict = model.decoder.state_dict()
   for key in decoder_dict.keys():
     decoder_dict[key] = decoder_dict[key].to(torch.device('cpu'))
   state_dict['decoder'] = decoder_dict
   
+  # save the model
   torch.save(state_dict, path)
 
+# load the states
 def load_AdaIN(path='AdaIN.pth', training_mode=False):
+  # load the states
   state_dict = torch.load(path)
+  # create a model, if there is encoder's state, do not used pretrained encoder from torchvision
   model = AdaIN(torchvision_encoder=('encoder' not in state_dict.keys()), training_mode=training_mode)
   try:
+    # load the states of encoder
     model.encoder.load_state_dict(state_dict['encoder'])
   except KeyError:
     pass
+  # load the states of decoder
   model.decoder.load_state_dict(state_dict['decoder'])
+
+  # return loaded model
   return model
